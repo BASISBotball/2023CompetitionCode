5a3128892dfc99d075ba9122453f7d9cdb666bf5
BASISBotball <98559830+BASISBotball@users.noreply.github.com>
2023-03-30 17:44:57 -0400

    Add files via upload

--- create_bot/poms.c
+++ create_bot/poms.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 void collect_poms(int speed, int dir /* 0 is left; 1 is right */, pom_selector_left, pom_selector_right, pom_selector){
-    int time = /*insert time here*/
+    int time = /*insert time here*/;
     if (dir == 1){
         set_servo_position(pom_selector, pom_selector_right);
         msleep(time);
@@ -14,7 +14,7 @@ void collect_poms(int speed, int dir /* 0 is left; 1 is right */, pom_selector_l
 }
 
 void deflect_poms(int speed, int dir /* 0 is left; 1 is right */, pom_deflector_left, pom_deflector_right, pom_deflector){
-    int time = /*insert time here*/
+    int time = /*insert time here*/;
     if (dir == 1){
         set_servo_position(pom_deflector, pom_deflector_right);
         msleep(time);
@@ -27,11 +27,11 @@ void deflect_poms(int speed, int dir /* 0 is left; 1 is right */, pom_deflector_
 
 int poms(int pom_selector, int pom_deflector){
     int speed = /* enter speed value here */;
-    int pom_deflector_neutral = /* enter pom deflector neutral position here */
-    int pom_selector_right= /* enter pom selector right position here */
-    int pom_selector_left= /* enter pom selector left position here */
-    int pom_deflector_right= /* enter pom deflector right position here */
-    int pom_deflector_left= /* enter pom deflector left position here */
+    int pom_deflector_neutral = /* enter pom deflector neutral position here */;
+    int pom_selector_right= /* enter pom selector right position here */;
+    int pom_selector_left= /* enter pom selector left position here */;
+    int pom_deflector_right= /* enter pom deflector right position here */;
+    int pom_deflector_left= /* enter pom deflector left position here */;
     create_connect();
     enable_servos();
     set_servo_position(pom_deflector, pom_deflector_neutral);
